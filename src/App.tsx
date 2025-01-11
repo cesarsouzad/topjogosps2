f6e0ca20d7e0af70ed33bb620f0ba21b3b490522
Cesar Jorge <cesarsouzad@gmail.com>
2025-01-11 05:24:35 -0300

    build react e correção de importação

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import logo from './assets/logops2.jpeg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import GameCard from './components/gamecard';
+import GameCard from './components/GameCard';
 
 type Game = {
   id: number;
